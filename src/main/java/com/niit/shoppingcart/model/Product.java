fb65c9fb8b3d4b52b0198bcba92474091df9c248
Pratiksha Patil <patilpratiksha1412@gmail.com>
2016-11-09 09:53:29 +0530

    this is my second commit

--- src/main/java/com/niit/shoppingcart/model/Product.java
+++ src/main/java/com/niit/shoppingcart/model/Product.java
@@ -5,6 +5,23 @@ import org.springframework.stereotype.Component;
 public class Product {
 	    
 	    private String proid;
+	    
+		public String getProid() {
+			return proid;
+		}
+
+		public void setProid(String proid) {
+			this.proid = proid;
+		}
+
+		public String getProname() {
+			return proname;
+		}
+
+		public void setProname(String proname) {
+			this.proname = proname;
+		}
+
 		private String proname;
 	}
 
